f7a637af0bf10d06ad843c99840b77dff6bffb60
Johannes Thiel <nhsbjojo@gmail.com>
2018-05-06 12:56:49 +0200

    Webpack + Vue + Sass + Pug config

---
+++ app/App.vue
@@ -0,0 +1,19 @@
+<template lang='pug'>
+  .app
+    h1 Hoho
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data () {
+    return {
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ config/webpack.common.js
@@ -0,0 +1,51 @@
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const WebpackBuildNotifierPlugin = require('webpack-build-notifier')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports =  {
+  entry: './app/index.js',
+  output: {
+    path: __dirname + '/../dist',
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue/,
+        exclude: /node_modules/,
+        loader: 'vue-loader',
+        options: {
+          extractCSS: true,
+          loaders: {
+              sass: 'vue-style-loader!css-loader!sass-loader?'+
+                  '{"includePaths":["app/sass"]}'
+          }
+        }
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-plain-loader',
+      },
+      {
+        test: /\.scss/,
+        loader: 'style-loader!css-loader!sass-loader',
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist'], {
+      root: __dirname + '/..',
+    }),
+    new HtmlWebpackPlugin({
+      template: 'app/index.html',
+    }),
+    new WebpackBuildNotifierPlugin({
+      title: "Github Issue Visualizer Webpack Build",
+      suppressSuccess: true
+    }),
+    new VueLoaderPlugin(),
+  ],
+}
