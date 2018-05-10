a871d7eca44054c76ddaefc957c798728ce264f0
Johannes Thiel <nhsbjojo@gmail.com>
2018-05-10 12:43:37 +0200

    Added a navbar component

---
+++ app/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template lang='pug'>
+  .navbar
+    input(type='text' placeholder='owner/repo' v-model='repository')
+    button(@click='loadRepository') Load Issues
+
+
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data () {
+    return {
+      repository: '',
+    }
+  },
+  methods: {
+    loadRepository () {
+      const params = this.repository.split('/')
+      const owner = params[0]
+      const repo = params[1]
+      // Load issues and labels
+      this.$store.dispatch('repository/loadIssues', { owner, repo })
+      this.$store.dispatch('repository/loadLabels', { owner, repo })
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
