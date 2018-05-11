238e78d4b6dc2fbbcacc890e75160dbc78a471d1
Johannes Thiel <nhsbjojo@gmail.com>
2018-05-11 10:15:34 +0200

    Added Buefy to the app

--- app/index.js
+++ app/index.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
+import Buefy from 'buefy'
 import App from './App.vue'
 
+import 'buefy/lib/buefy.css'
+
 import store  from './store'
+
+Vue.use(Buefy)
 
 const vueApp = new Vue({
   el: '#app',
--- config/webpack.common.js
+++ config/webpack.common.js
@@ -39,6 +39,10 @@ module.exports =  {
       {
         test: /\.scss/,
         loader: 'style-loader!css-loader!sass-loader',
+      },
+      {
+        test: /\.css/,
+        loader: 'style-loader!css-loader',
       }
     ]
   },
